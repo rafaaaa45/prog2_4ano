126e3033a4b4a41da605b0ff4124d641a0540ddd
rafaaaa45 <rmonteiro811@gmail.com>
2023-11-18 23:22:07 +0000

    Initial commit

---
+++ src/main/java/com/example/prog2_4ano/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.prog2_4ano;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
